29d804addd3faefcef35201a540d3e3f23fab6be
Girraj singhal <72375025+girraj12@users.noreply.github.com>
2024-04-01 10:37:00 +0530

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,11 +2,10 @@
 import { Box, styled, Typography, Link } from '@mui/material';
 import {LinkedIn, Email } from '@mui/icons-material';
 const Banner = styled(Box)`
-    background-image: url(https://images.pexels.com/photos/618613/pexels-photo-618613.jpeg?auto=compress&cs=tinysrgb&w=600
-);
+    background-image: url('https://images.pexels.com/photos/265072/pexels-photo-265072.jpeg?auto=compress&cs=tinysrgb&w=600');
     width: 100%;
-    height: 50vh;
-    background-position: left 0px top -100px;
+    height: 58vh;
+    background-position: left 0px top -400px;
     background-size: cover;
 `;
 
